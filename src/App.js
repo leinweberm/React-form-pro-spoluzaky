84a472bd74d6e4ad33f5fb54245e1ff5c5b9e19c
Martin Leinweber <102190637+leinweberm@users.noreply.github.com>
2022-06-21 21:10:17 +0200

    Funkci commit

---
+++ src/App.js
@@ -0,0 +1,192 @@
+import './App.css';
+import { FormSection, Formular, InputDiv, MainTitle, PageContainer, SectionTitle } from './AppStyles';
+//
+import { useReducer, useState, useEffect } from 'react';
+// initialState pro useReducer
+const defaultObjednavka = {
+  typ: '',
+  hmotnost: 0,
+  doprava: 0,
+  bioKvalita: false,
+  premiumKvalita: false,
+  chudiKvalita: false,
+  darkoveBaleni: false,
+  rozpocet: 0,
+  email: '',
+};
+// reducer funkce pro useReducer
+function setObjednavka(objednavka, action) {
+  switch (action.type) {
+    case "update_text":
+      return { ...objednavka, [action.key]: action.value };
+    case "update_number":
+      return { ...objednavka, [action.key]: parseFloat(action.value) };
+    case "toggle_bio": return { ...objednavka, bioKvalita: !objednavka.bioKvalita };
+    case "toggle_premium": return { ...objednavka, premiumKvalita: !objednavka.premiumKvalita };
+    case "toggle_chudi": return { ...objednavka, chudiKvalita: !objednavka.chudiKvalita };
+    case "toggle_darek": return { ...objednavka, darkoveBaleni: !objednavka.darkoveBaleni };
+    default: return objednavka;
+  }
+};
+
+function App() {
+  const [finalPrice, setFinalPrice] = useState(0);
+  const [showFinalPrice, setShowFinalPrice] = useState(0);
+  //state objednavky spravujeme pomoci useReducer hooku
+  const [objednavka, dispatch] = useReducer(setObjednavka, defaultObjednavka);
+
+  // useEffect(() => { console.log(JSON.stringify(objednavka)) }, [objednavka]);
+  useEffect(() => {
+    let newFinalPrice = getFinalPrice(objednavka);
+    setShowFinalPrice(newFinalPrice);
+  }, [objednavka]);
+
+  const getFinalPrice = (objednavka) => {
+    let thisBasePrice = objednavka.typ * objednavka.hmotnost;
+    let thisFinalPrice = thisBasePrice;
+    if (objednavka.doprava === 250) {
+      thisFinalPrice += 250;
+    }
+    else if (objednavka.doprava === 0.10) {
+      thisFinalPrice += (thisBasePrice * 0.10);
+    }
+    if (objednavka.bioKvalita) { thisFinalPrice += (thisBasePrice * 0.30) };
+    if (objednavka.premiumKvalita) { thisFinalPrice += (thisBasePrice * 0.50) };
+    if (objednavka.chudiKvalita) { thisFinalPrice -= (thisBasePrice * 0.15) };
+    if (objednavka.darkoveBaleni) { thisFinalPrice += 500 };
+    setFinalPrice(thisFinalPrice);
+    return thisFinalPrice;
+
+  }
+
+  return (
+    <PageContainer>
+      <Formular>
+        <FormSection name="nadpis"><MainTitle>Vaše objednávka</MainTitle></FormSection>
+        <FormSection name="vyber">
+          <SectionTitle>Výběr krmiva</SectionTitle>
+          {/* VYBER TYPU KRMIVA */}
+          <label>Výběr typu krmiva:</label>
+          <select id='typ' onClick={(e) => {
+            dispatch({
+              type: "update_text",
+              value: e.target.value,
+              key: "typ",
+            });
+          }}>
+            <option value={0}>nevybráno</option>
+            <option value={150}>psí žrádlo 150kč/kg</option>
+            <option value={120}>kočičí žrádlo 120kč/kg</option>
+            <option value={50}>žrádlo pro rybičky 50kč/kg</option>
+            <option value={800}>žrádlo pro tygra 800kč/kg</option>
+          </select>
+          {/* HMOTNOST KRMIVA */}
+          <label>Hmotnost krmiva (kg):</label>
+          <input type="text" id="hmotnost" value={objednavka.hmotnost} onChange={(e) => {
+            dispatch({
+              type: "update_number",
+              value: e.target.value,
+              key: "hmotnost",
+            });
+          }} />
+        </FormSection>
+        {/* VLASTNOSTI / EXTRAS */}
+        <FormSection name="vlastnosti">
+          <SectionTitle>Vlastnosti krmiva</SectionTitle>
+          <InputDiv>
+            <input type="checkbox" id="bioKvalita" onChange={(e) => {
+              dispatch({
+                type: "toggle_bio",
+              });
+            }} />
+            <label>Bio kvalita (+30%)</label>
+          </InputDiv>
+          <InputDiv>
+            <input type="checkbox" id="premiumKvalita" onChange={(e) => {
+              dispatch({
+                type: "toggle_premium",
+              });
+            }} />
+            <label>Extra prémium kvalita (+50%)</label>
+          </InputDiv>
+          <InputDiv>
+            <input type="checkbox" id="chudaKvalita" onChange={(e) => {
+              dispatch({
+                type: "toggle_chudi",
+              });
+            }} />
+            <label>Extra nekvalitní pro chudé (-15%)</label>
+          </InputDiv>
+          <InputDiv>
+            <input type="checkbox" id="darkoveBaleni" onChange={() => {
+              dispatch({
+                type: "toggle_darek",
+              });
+            }} />
+            <label>Dárkové balení (+500kč)</label>
+          </InputDiv>
+        </FormSection>
+        <FormSection name="doprava">
+          {/* DOPRAVA */}
+          <SectionTitle>Doprava</SectionTitle>
+          <div>
+            <input type="radio" name="doprava" id="dopravaOdber" value={0} onChange={(e) => {
+              dispatch({
+                type: "update_number",
+                value: e.target.value,
+                key: "doprava",
+              });
+            }} />
+            <label>osobní odběr (zdarma)</label>
+            <input type="radio" name="doprava" id="dopravaFiremniKuryr" value={0.10} onChange={(e) => {
+              dispatch({
+                type: "update_number",
+                value: e.target.value,
+                key: "doprava",
+              });
+            }} />
+            <label>firemní kurýr (+10%)</label>
+            <input type="radio" name="doprava" id="dopravaCeskaPosta" value={250} onChange={(e) => {
+              dispatch({
+                type: "update_number",
+                value: e.target.value,
+                key: "doprava",
+              });
+            }} />
+            <label>Česká Pošta (+250kč)</label>
+          </div>
+        </FormSection>
+        <FormSection name="kalkulace">
+          {/* FINALNI CENA - ZOBRAZENI + ROZPOCET */}
+          <SectionTitle>Konečná kalkulace</SectionTitle>
+          <label>Zadejte váš rozpočet:</label>
+          <input type="text" id="rozpocet" value={objednavka.rozpocet} onChange={(e) => {
+            dispatch({
+              type: "update_number",
+              value: e.target.value,
+              key: "rozpocet",
+            });
+          }} />
+          <label>Finální cena:</label>
+          <input type="text" id="finalniCena" value={showFinalPrice} disabled />
+          <div style={{ marginTop: '20px', background: 'lightGray', border: '1px solid black', cursor: 'pointer', justifyContent: 'center', alignItems: 'center', display: 'flex', backgroundColor: 'white' }} onClick={() => {
+          }}>
+            Kontrola
+          </div>
+        </FormSection>
+        <FormSection name="email">
+          <SectionTitle>Kontaktní údaje</SectionTitle>
+          <label>Zadejte vaši emailovou adresu</label>
+          <input type="text" id="email" value={objednavka.email} onChange={(e) => {
+            dispatch({
+              type: "update_text",
+              value: e.target.value,
+              key: "email",
+            });
+          }} />
+        </FormSection>
+      </Formular>
+    </PageContainer>
+  );
+};
+export default App;
---
+++ src/AppStyles.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+   display: flex;
+   max-width: 1200px;
+   width: 100%;
+   min-height: 100vh;
+   justify-content: center;
+   align-items: center;
+   align-self: center;
+   justify-self: center;
+`;
+export const Formular = styled.form`
+   width: 800px;
+   min-height: 800px;
+   border: 1px solid black;
+   padding: 20px;
+   display: grid;
+   grid-template-columns: 1fr;
+   grid-template-areas:
+      'nadpis'
+      'vyber'
+      'vlastnosti'
+      'doprava'
+      'email'
+      'kalkulace';
+   gap: 20px;
+`;
+export const FormSection = styled.div`
+   display: flex;
+   flex-direction: column;
+   width: 100%;
+   background-color: lightblue;
+   padding: 20px;
+   &:nth-child(1){
+      grid-area: nadpis;
+   }
+   &:nth-child(2){
+      grid-area: vyber;
+   }
+   &:nth-child(3){
+      grid-area: vlastnosti;
+   }
+   &:nth-child(4){
+      grid-area: doprava;
+   }
+   &:nth-child(5){
+      grid-area: kalkulace;
+   }
+   &:nth-child(6){
+      grid-area: email;
+   }
+`;
+export const SectionTitle = styled.h2`
+   color: black;
+   font-size: 20px;
+   margin: 0px;
+   padding: 0;
+   padding-bottom: 10px;
+`;
+export const MainTitle = styled(SectionTitle)`
+   font-size: 30px;
+   align-self: center;
+   justify-self: center;
+`;
+export const InputDiv = styled.div`
+   display: flex;
+   flex-direction: row;
+   justify-content: flex-start;
+   align-items: center;
+`;
